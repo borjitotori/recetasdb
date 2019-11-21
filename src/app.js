c296894efaa24831c5626b03030aec18b0f9afe9
borjitotori <33838047+borjitotori@users.noreply.github.com>
2019-11-21 15:44:38 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import { GraphQLServer } from "graphql-yoga";
 
 import "babel-polyfill";
 
-const usr = "avalero";
-const pwd = "123456abc";
-const url = "cluster0-e8ug9.mongodb.net/test?retryWrites=true&w=majority";
+const usr = "bvillarreal";
+const pwd = "123";
+const url = "cluster0-qr8a1.mongodb.net/test?retryWrites=true&w=majority";
 
 /**
  * Connects to MongoDB Server and returns connected client
@@ -33,58 +33,159 @@ const runGraphQLServer = function(context) {
     type Query{
       getAuthor(id: ID!): Author
       getAuthors: [Author]!
+      getRecipes: [Recipe]!
+      getIngredients: [Ingredient]!
     }
-
     type Mutation{
-      addAuthor(name: String!, age: Int!):Author!
+      addAuthor(name: String!, email: String!): Author!
+      addIngredient(name: String!): Ingredient!
+      addRecipe(name: String!, description: String!, author: ID!, ingredients: [ID!]): Recipe!
     }
-
-    type Author{
-      id: ID!
+    type Recipe {
+      _id: ID!
       name: String!
-      age: Int!
+      description: String!
+      author: Author!
+      ingredients: [Ingredient!]
+    }
+    type Author {
+      _id: ID!
+      name: String!
+      email: String!
+      recipes: [Recipe]!
+    }
+    type Ingredient {
+      _id: ID!
+      name: String!
+      recipes: [Recipe]!
     }
     `;
 
   const resolvers = {
+    Recipe: {
+      author: async(parent,args,ctx,info) => {
+        const author = parent.author;
+        const {client} = ctx;
+        const db = client.db("recetario");
+        const collection = db.collection("authors");
+        const result = await collection.findOne({_id: ObjectID(author)});
+        return result;
+      },
+      ingredients: async(parent, args, ctx, info) =>{
+        const ingredients = parent.ingredients.map(elem => ObjectID(elem));
+        const {client} = ctx;
+        const db = client.db("recetario");
+        const collection = db.collection("ingredients");
+        const result = await collection.find({_id:{$in: ingredients}}).toArray();
+        return result;
+      }
+    },
+    Author: {
+      recipes: async(parent,args,ctx,info) => {
+        const author = parent._id;
+        const {client} = ctx;
+        const db = client.db("recetario");
+        const collection = db.collection("recetas");
+        const result = await collection.find({author: author}).toArray();
+        return result;
+      } 
+    },
+    Ingredient: {
+      recipes:async(parent,args,ctx,info) => {
+        const ingredient = parent._id;
+        const {client} = ctx;
+        const db = client.db("recetario");
+        const collection = db.collection("recetas");
+        const result = await collection.find({ingredients: ingredient}).toArray();
+        return result;
+      } 
+    },
     Query: {
       getAuthor: async (parent, args, ctx, info) => {
-        const { id } = args;
+        const { _id } = args;
         const { client } = ctx;
-        const db = client.db("blog");
+        const db = client.db("recetario");
         const collection = db.collection("authors");
-        const result = await collection.findOne({ _id: ObjectID(id) });
+        const result = await collection.findOne({ _id: ObjectID(_id) });
         return result;
       },
       getAuthors: async (parent, args, ctx, info) => {
         const { client } = ctx;
-        const db = client.db("blog");
+        const db = client.db("recetario");
         const collection = db.collection("authors");
         const result = await collection.find({}).toArray();
         return result;
-      }
+      },
+      getRecipes: async (parent, args, ctx, info) => {
+        const { client } = ctx;
+        const db = client.db("recetario");
+        const collection = db.collection("recetas");
+        const result = await collection.find({}).toArray();
+        return result;
+      },
+      getIngredients: async (parent, args, ctx, info) => {
+        const { client } = ctx;
+        const db = client.db("recetario");
+        const collection = db.collection("ingredients");
+        const result = await collection.find({}).toArray();
+        return result;
+      },
     },
     Mutation: {
       addAuthor: async (parent, args, ctx, info) => {
-        const { name, age } = args;
+        const { name, email } = args;
         const { client } = ctx;
 
-        const db = client.db("blog");
+        const db = client.db("recetario");
         const collection = db.collection("authors");
-        const result = await collection.insertOne({ name, age });
+        const result = await collection.insertOne({ name, email });
 
         return {
           name,
-          age,
-          id: result.ops[0]._id
+          email,  
+          _id: result.ops[0]._id
         };
-      }
+      },      
+      addIngredient: async(parent, args, ctx, info) => {
+        const { name } = args;
+        const { client } = ctx;
+
+        const db = client.db("recetario");
+        const collection = db.collection("ingredients");
+        const result = await collection.insertOne({name});
+
+        return {
+          name,
+          _id: result.ops[0]._id
+        }
+      },
+      addRecipe: async(parent, args, ctx, info) => {
+        const {name, description, author, ingredients} = args;
+        const { client } = ctx;
+
+        const db = client.db("recetario");
+        const collection = db.collection("recetas")
+        const result = await collection.insertOne({
+          name, 
+          description, 
+          author: ObjectID(author), 
+          ingredients: ingredients.map(elem => ObjectID(elem))
+        });
+
+        return{
+          name,
+          description,
+          author: ObjectID(author),
+          ingredients: ingredients.map(elem => ObjectID(elem)),
+          _id: result.ops[0]._id
+        }
+      },
     }
   };
 
   const server = new GraphQLServer({ typeDefs, resolvers, context });
   const options = {
-    port: 8000
+    port: 4000
   };
 
   try {
@@ -105,6 +206,7 @@ const runApp = async function() {
   try {
     runGraphQLServer({ client });
   } catch (e) {
+    console.log(e)
     client.close();
   }
 };
